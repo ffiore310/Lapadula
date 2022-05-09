ebb104fcc9f8ab9478df9883d1e55607604cf7ff
Fernando Fiore <fernandoof@al.insper.edu.br>
2022-05-09 17:15:25 -0300

    funcao haversine

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,12 @@
+import math
+
+def haversine(r, e1, h1, e2, h2):
+    x = (math.sin(math.radians((e2 - e1)/2)))**2
+    y = math.cos(math.radians(e1))*math.cos(math.radians(e2))*(math.sin(math.radians((h2 - h1)/2)))**2
+    z = math.sqrt(x+y)
+    d = 2*r*math.asin(z)
+    return d
+
 def normaliza (d1):
     nd = {}
     for continente, paises in d1.items():
