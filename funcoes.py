4c3e856924c96e403ea579d388f698070e3558e1
Arthur Calabresi <arthurec@al.insper.edu.br>
2022-05-09 17:12:30 -0300

    funcao normaliza

---
+++ funcoes.py
@@ -0,0 +1,8 @@
+def normaliza (d1):
+    nd = {}
+    for continente, paises in d1.items():
+        for pais, caracteristicas in paises.items():
+            nd[pais] = caracteristicas 
+            nd[pais]['continente'] = continente
+    return nd
+
