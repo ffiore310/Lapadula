60b22c5ca7a335504c6d6bb92a45e1e3ef16accf
Arthur Calabresi <arthurec@al.insper.edu.br>
2022-05-11 18:22:10 -0300

    banco de dados

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# BANCO DE DADOS
+
 # ABERTURA JOGO
 
 print('  =============================  \n|                               |\n|  Bem-vindo ao Insper Paises!  |\n|                               |\n  ==== Design de Software =====\n \n Comandos:\n     dica       - entra no mercado de dicas\n     desisto    - desiste da rodada\n     inventario - exibe sua posicao')
