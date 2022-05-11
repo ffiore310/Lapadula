e12175869b5e0bae4ca730d2bad0c29d605a2d12
Arthur Calabresi <arthurec@al.insper.edu.br>
2022-05-11 10:51:01 -0300

    main- abertura jogo

--- rascunho_dev02.py
+++ rascunho_dev02.py
@@ -1,2 +1,11 @@
 # RASCUNHO DESENVOLVEDOR 2 - ARTHUR CALABRESI
 
+# ABERTURA JOGO
+
+print('  =============================  \n|                               |\n|  Bem-vindo ao Insper Paises!  |\n|                               |\n  ===== Design de Software =====\n \n Comandos:\n     dica       - entra no mercado de dicas\n     desisto    - desiste da rodada\n     inventario - exibe sua posicao')
+# MERCADO DE DICA
+
+print('Mercado de Dicas \n ------------------------------------------\n 0. Sem dica\n 1. Cor da bandeira    - custa 4 tentativas\n 2. Letra da capital   - custa 3 tentativas\n 3. Area               - custa 6 tentativas\n 4. Populacao          - custa 5 tentativas\n 5. Continente         - custa 7 tentativas\n ------------------------------------------')
+opcao = int(input('Escolha sua opcao [0|1|2|3|4|5]: '))
+
+
