a007bb2298f9249f8bf8c624da0e8fbf3dc96ec8
Fernando Fiore <fernandoof@al.insper.edu.br>
2022-05-12 16:33:40 -0300

    mercado

--- racunho_dev01.py
+++ racunho_dev01.py
@@ -3853,14 +3853,7 @@ while roda_while:
         roda_while = False
 
     elif resposta == 'dica':
-        dicas = {}
-        print('Mercado de Dicas \n ------------------------------------------\n 0. Sem dica\n 1. Cor da bandeira    - custa 4 tentativas\n 2. Letra da capital   - custa 3 tentativas\n 3. Area               - custa 6 tentativas\n 4. Populacao          - custa 5 tentativas\n 5. Continente         - custa 7 tentativas\n ------------------------------------------')
-        opcao = int(input('Escolha sua opcao [0|1|2|3|4|5]: '))
-        if opcao == 3:
-            for e, i in dicionario_paises.items():
-                area = i['area']
-                dicas['Área'] = area
-                print(dicas)
+        print('a')
         
 
     elif resposta == 'inventario':
