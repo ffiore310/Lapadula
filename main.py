face1a75a3e05a17674f0eb686bbc46799ddafa4
Arthur Calabresi <arthurec@al.insper.edu.br>
2022-05-12 17:24:48 -0300

    Loop deseja jogar novamente

--- main.py
+++ main.py
@@ -3819,87 +3819,112 @@ DADOS = {
 raio_Terra = 6371
 
 # ABERTURA JOGO
+print('  =============================  \n|                               |\n|  Bem-vindo ao Insper Paises!  |\n|                               |\n  ==== Design de Software =====\n \n Comandos:\n     dica       - entra no mercado de dicas\n     desisto    - desiste da rodada\n     inventario - exibe sua posicao\n')
+inicializa = True
 
-dicionario_paises = normaliza(DADOS)
+while inicializa:
+  
+  dicionario_paises = normaliza(DADOS)
 
-cont_sorteado = sorteia_pais(DADOS)
-pais_sorteado = sorteia_pais(DADOS[cont_sorteado])
-lista_inventario = []
-lista_paises = []
-tentativas = 20
+  cont_sorteado = sorteia_pais(DADOS)
+  pais_sorteado = sorteia_pais(DADOS[cont_sorteado])
+  lista_inventario = []
+  lista_paises = []
+  tentativas = 20
 
-roda_while = True
+  roda_while = True
 
-print('  =============================  \n|                               |\n|  Bem-vindo ao Insper Paises!  |\n|                               |\n  ==== Design de Software =====\n \n Comandos:\n     dica       - entra no mercado de dicas\n     desisto    - desiste da rodada\n     inventario - exibe sua posicao\n \nUm pais foi escolido! \nTente adivinhar! \nVoce tem {0} tentativa(s) '.format(tentativas))
+  print('Um pais foi escolido! \nTente adivinhar! \nVoce tem {0} tentativa(s)\n'.format(tentativas))
 
-while roda_while:
-    
-    resposta = input('Qual o seu palpite?: ')
-
-   # ACERTOU O JOGO
-    if resposta.lower() == pais_sorteado:
-        print('Parabens! Voce acertou em {} tentativa(s)'.format(20-tentativas+1))
-        roda_while = False
-
-  # DESISTIU DO JOGO
-
-    elif resposta == 'desisto':
-            certeza = input('Tem certeza que deseja desistir da rodada? [s|n]')
-            if certeza == 's':
-                roda_while = False
-                print('Que deselegante desistir, o pais era: {}'.format(pais_sorteado))
-                print('Ate a proxima!')
-            
-    # ACABAR AS TENTATIVAS
-
-    elif tentativas == 1:
-        print('Voce perdeu, o pais era: {}'.format(pais_sorteado))
-        roda_while = False
-
-  # MERCADO DE DICAS
-
-    elif resposta == 'dica':
-        print('dica')
-
-  # PRINT INVENTARIO
-
-    elif resposta == 'inventario':
-        inventario = tabela_distancias(lista_inventario)
-        print('\nInventario :\n {}'.format(inventario))
-
-  # CHUTE FOR DIFERENTE
-
-    elif resposta.lower() in dicionario_paises:
-
-      #CALCULAR AS DISTANCIAS
-
-        lat1 = dicionario_paises[resposta.lower()]['geo']['latitude']
-        long1 = dicionario_paises[resposta.lower()]['geo']['longitude'] 
-        lat2 = dicionario_paises[pais_sorteado]['geo']['latitude']
-        long2 = dicionario_paises[pais_sorteado]['geo']['longitude']
-        distancia = haversine(raio_Terra, lat1, long1, lat2, long2)
-        distancia = int(distancia)
+  while roda_while:
       
-      # PAIS JA FOI CHUTADO
+      resposta = input('Qual o seu palpite?: ')
 
-        if resposta.lower() in lista_paises:
-            inventario = tabela_distancias(lista_inventario)
-            print('\nInventario :\n {}'.format(inventario))
-            print('Este pais ja foi inserido! Insira um novo')
-            print('Você tem {} tentativa(s)'.format(tentativas))
+    # ACERTOU O JOGO
+      if resposta.lower() == pais_sorteado:
+          print('Parabens! Voce acertou em {} tentativa(s)'.format(20-tentativas+1))
+          roda_while = False
 
-      # PAIS NAO FOI CHUTADO
-            
-        else:
-            lista_paises.append(resposta.lower())
-            tentativas -= 1
-            lista_inventario = adiciona_em_ordem(resposta.lower(), distancia, lista_inventario)
-            inventario = tabela_distancias(lista_inventario)
-            print('\nInventario :\n {}'.format(inventario))
-            print('Você tem {} tentativa(s)'.format(tentativas))
+    # DESISTIU DO JOGO
 
-# SE O PAIS FOR DESCONHECIDO
+      elif resposta == 'desisto':
+              certeza = input('Tem certeza que deseja desistir da rodada? [s|n]')
+              if certeza == 's':
+                  roda_while = False
+                  print('Que deselegante desistir, o pais era: {}'.format(pais_sorteado))
 
-    else:
-        print('País desconhecido')
+                  a = True
+                  while a:
+                    novamente = input('\nDeseja jogar novamente? [s|n]')
+                    if novamente == 'n':
+                      roda_while = False
+                      inicializa = False
+                      a = False
+                    elif novamente != 's':
+                      print('Insira apenas [s|n]')
+
+              
+      # ACABAR AS TENTATIVAS
+
+      elif tentativas == 1:
+          print('Voce perdeu, o pais era: {}'.format(pais_sorteado))
+          roda_while = False
+          a = True
+          while a:
+            novamente = input('\nDeseja jogar novamente? [s|n]')
+            if novamente == 'n':
+              roda_while = False
+              inicializa = False
+              a = False
+            elif novamente != 's':
+               print('Insira apenas [s|n]')
+
+    # MERCADO DE DICAS
+
+      elif resposta == 'dica':
+          print('dica')
+
+    # PRINT INVENTARIO
+
+      elif resposta == 'inventario':
+          inventario = tabela_distancias(lista_inventario)
+          print('\nInventario :\n {}'.format(inventario))
+
+    # CHUTE FOR DIFERENTE
+
+      elif resposta.lower() in dicionario_paises:
+
+        #CALCULAR AS DISTANCIAS
+
+          lat1 = dicionario_paises[resposta.lower()]['geo']['latitude']
+          long1 = dicionario_paises[resposta.lower()]['geo']['longitude'] 
+          lat2 = dicionario_paises[pais_sorteado]['geo']['latitude']
+          long2 = dicionario_paises[pais_sorteado]['geo']['longitude']
+          distancia = haversine(raio_Terra, lat1, long1, lat2, long2)
+          distancia = int(distancia)
         
+        # PAIS JA FOI CHUTADO
+
+          if resposta.lower() in lista_paises:
+              inventario = tabela_distancias(lista_inventario)
+              print('\nInventario :\n {}'.format(inventario))
+              print('Este pais ja foi inserido! Insira um novo')
+              print('Você tem {} tentativa(s)'.format(tentativas))
+
+        # PAIS NAO FOI CHUTADO
+              
+          else:
+              lista_paises.append(resposta.lower())
+              tentativas -= 1
+              lista_inventario = adiciona_em_ordem(resposta.lower(), distancia, lista_inventario)
+              inventario = tabela_distancias(lista_inventario)
+              print('\nInventario :\n {}'.format(inventario))
+              print('Você tem {} tentativa(s)'.format(tentativas))
+
+  # SE O PAIS FOR DESCONHECIDO
+
+      else:
+          print('País desconhecido')
+
+
+print('Que pena! Espero que tenha se divertido!\n Ate a proxima ;)')
