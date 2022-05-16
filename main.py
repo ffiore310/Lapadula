f160ba32793f0fe84e3610b8483e8afc4d359385
Arthur Calabresi <arthurec@al.insper.edu.br>
2022-05-15 21:20:18 -0300

    passando pro main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # BANCO DE DADOS
+from random import choice
 from funcaocolorida import tabela_distancia_colorida
-from funcoes import adiciona_em_ordem, normaliza, sorteia_pais, haversine, tabela_distancias
+from funcoes import adiciona_em_ordem, coloca_na_lista, normaliza, sorteia_letra, sorteia_pais, haversine, tabela_distancias, mercado_dicas, tabela_dica
 from dados import DADOS
 
 raio_Terra = 6371
@@ -17,11 +18,29 @@ while inicializa:
   pais_sorteado = sorteia_pais(DADOS[cont_sorteado])
   lista_inventario = []
   lista_paises = []
+  lista_cores = []
+  lista_letras = []
+  lista_letras_usadas = []
+  lista_cores_bandeira = []
+  lista_area = []
+  lista_cont = []
+  lista_pop = []
   tentativas = 20
+  contador_cores = 0
+  contador_letras = 0
+  contador_area = 0
+  contador_pop = 0
+  contador_cont = 0
+  cores = dicionario_paises[pais_sorteado]['bandeira']
+  for cor, i in cores.items():
+    if cor != 'outras' and i != 0:
+      lista_cores_bandeira.append(cor)
+  capital = dicionario_paises[pais_sorteado]['capital']
 
   roda_while = True
-  print(pais_sorteado)
-  print('Um pais foi escolhido! \nTente adivinhar! \nVoce tem {0} tentativa(s)\n'.format(tentativas))
+  
+  print('  =============================  \n|                               |\n|  Bem-vindo ao Insper Paises!  |\n|                               |\n  ==== Design de Software =====\n \n Comandos:\n     dica       - entra no mercado de dicas\n     desisto    - desiste da rodada\n     inventario - exibe sua posicao\n \nUm pais foi escolhido! \nTente adivinhar! \nVoce tem {0} tentativa(s) '.format(tentativas))
+
 
   while roda_while:
       
@@ -66,7 +85,7 @@ while inicializa:
               
       # ACABAR AS TENTATIVAS
 
-      elif tentativas == 1:
+      elif tentativas == 1 :
           print('Vixe! Parece que acabaram as tentativas :\ \n Voce perdeu, o pais era: {}'.format(pais_sorteado))
           roda_while = False
           a = True
@@ -84,13 +103,71 @@ while inicializa:
     # MERCADO DE DICAS
 
       elif resposta == 'dica':
-          print('dica')
+        print(mercado_dicas(tentativas, contador_pop, capital, contador_cores, contador_letras, lista_cores_bandeira, contador_area, contador_cont ))
+        opcao = int(input('Escolha sua opcao [0|1|2|3|4|5]: '))
+
+        if opcao == 0:
+            inventario = tabela_distancia_colorida(lista_inventario)
+            print('\nInventario :\n {}'.format(inventario))
+            print(tabela_dica(lista_letras,lista_cores,lista_area, lista_cont, lista_pop))
+
+        elif opcao == 2 and contador_cores != len(lista_cores_bandeira):
+            tentativas -= 4
+            contador_cores += 1
+            cor_sorteada = choice(lista_cores_bandeira)
+            if len(lista_cores) == 0:
+              lista_cores = ['Cores da bandeira', [cor_sorteada]]
+
+            elif cor_sorteada not in lista_cores:
+              lista_cores[1].append(cor_sorteada)
+            lista_cores_bandeira.remove(cor_sorteada)
+            print(tabela_dica(lista_letras,lista_cores,lista_area, lista_cont, lista_pop))
+
+        elif opcao == 1 and contador_letras != len(capital):
+            contador_letras += 1
+            tentativas -= 3
+            letra_sorteada = sorteia_letra(capital, lista_letras_usadas)
+            if lista_letras == []:
+                lista_letras = ['Letras da capital', [letra_sorteada]]
+            else:
+                lista_letras[1].append(letra_sorteada)
+            lista_letras_usadas.append(letra_sorteada)
+            print(tabela_dica(lista_letras,lista_cores,lista_area, lista_cont, lista_pop))
+            
+            
+
+        elif opcao == 4 and contador_area != 1:
+            contador_area += 1
+            tentativas -= 6
+            area = dicionario_paises[pais_sorteado]['area']
+            lista_area = ['Area', area]
+            print(tabela_dica(lista_letras,lista_cores,lista_area, lista_cont, lista_pop))
+
+        elif opcao == 3 and contador_pop != 1:
+            contador_pop += 1
+            tentativas -= 5
+            populacao = dicionario_paises[pais_sorteado]['populacao']
+            lista_pop = ['Populacao', populacao]
+            print(tabela_dica(lista_letras,lista_cores,lista_area, lista_cont, lista_pop))
+
+        elif opcao == 5 and contador_cont != 1:
+            contador_cont += 1
+            tentativas -= 7
+            cont = dicionario_paises[pais_sorteado]['continente']
+            lista_cont = ['Continente', cont]
+            print(tabela_dica(lista_letras,lista_cores,lista_area, lista_cont, lista_pop))
+
+        else:
+          print('Opção inválida')
+
+        print('Você tem {} tentativa(s)'.format(tentativas))
 
     # PRINT INVENTARIO
 
       elif resposta == 'inventario':
-          inventario = tabela_distancia_colorida(lista_inventario)
-          print('\nDistancias :\n {}'.format(inventario))
+        inventario = tabela_distancia_colorida(lista_inventario)
+        print('\nInventario :\n {}'.format(inventario))
+        print('Dicas:\n {}'.format(tabela_dica(lista_letras,lista_cores,lista_area, lista_cont, lista_pop)))
 
     # CHUTE FOR DIFERENTE DA RESPOSTA
 
